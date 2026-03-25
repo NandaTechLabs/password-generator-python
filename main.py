c8b64036601a3ad042bce36e5942d815514c48d1
Fernanda Marques <nandacoder.dev@gmail.com>
2026-03-25 08:47:37 -0300

    Melhoria: uso de secrets e validação de senha forte

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-import random 
+import secrets
 import string
 
-def gerar_senha(tamanho=12):
+def gerar_senha_segura(tamanho=12):
     caracteres = string.ascii_letters + string.digits + string.punctuation
-    senha= ''.join(random.choice(caracteres) for _ in range(tamanho))
-    return senha
+    
+    while True:
+        senha = ''.join(secrets.choice(caracteres) for _ in range(tamanho))
+        
+        if (any(c.isdigit() for c in senha) and
+            any(c in string.punctuation for c in senha) and
+            any(c.isupper() for c in senha)):
+            return senha
 
 def verificar_forca(senha):
-    if len(senha) <8:
+    if len(senha) < 8:
         return "Fraca"
     elif any(c.isdigit() for c in senha) and any(c in string.punctuation for c in senha):
         return "Forte"
     else:
         return "Média"
-    
+
 def main():
     print("=== Gerador de Senhas ===")
 
     try:
-        tamanho = int(input("Digite o tamanho da senha: ")) 
-        senha = gerar_senha(tamanho)
+        tamanho = int(input("Digite o tamanho da senha: "))
+
+        if tamanho < 8:
+            print("⚠️ O ideal é usar pelo menos 8 caracteres.")
+
+        senha = gerar_senha_segura(tamanho)
 
         print(f"\nSenha gerada: {senha}")
         print(f"Força da senha: {verificar_forca(senha)}")
 
     except ValueError:
-            print("Por favor, digite um número válido.")
-        
-if __name__ == "__main__":   
+        print("Por favor, digite um número válido.")
+
+if __name__ == "__main__":
     main()
